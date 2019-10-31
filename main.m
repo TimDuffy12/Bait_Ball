e1192b206dc109a19a6efeb09b3d0a3d4d8294c0
Tim Duffy <timothy.duffy1@snhu.edu>
2019-10-31 10:43:10 -0400

    It works

--- main.m
+++ main.m
@@ -1,31 +1,42 @@
-coords = (10*rand(10,2) - 5);
+m = 10; %number of fish
+rho = 0.1;
 
+coords = (10*rand(m,2) - 5);
+newCoords = [];
+
+for i=1:m
 %K nearest
 k = 6;
 Idx = knearest(coords, k);
-n = size(Idx(1,:));
+Idx = Idx(i,:);
+%n = size(Idx(1,:));
 
-%translate to origin
-for i=1:6
-   C = [];
-   A = coords(Idx(i,:),:); %neighbors
-   B = A - A(1,:);         %shifted to origin
-   C = [C;B]
-end
+%translate to origin 
+A = coords(Idx(1,:),:); %neighbors
+B = A - A(1,:);         %shifted to origin
+B = B(2:6,:);
 
 %fishInts
-
+intervals = fishInts(B,rho);
 
 %orderedPairs
-
+intervals = orderedPairs(intervals);
 
 %combineInts
-
+intervals = combineInts(intervals);
 
 %intFix
-
+intervals = intFix(intervals);
 
 %newDirection
-
+direction = newDirection(intervals);
 
 %New Coords
+newCoords = [newCoords; direction + coords(i,:)];
+
+end
+
+%coords = newCoords;
+scatter(coords(:,1), coords(:,2))
+hold on
+scatter(newCoords(:,1), newCoords(:,2))
--- newDirection.m
+++ newDirection.m
@@ -1,9 +1,9 @@
 function dir = newDirection(ints)
 
-n = size(ints, 1)
+n = size(ints, 1);
 
-dir = sum([cos(ints(:,1)) sin(ints(:,2))])
+dir = sum([cos(ints(:,1)) sin(ints(:,2))]);
 
-dir = dir./n
+dir = dir./n;
 
 end
